efc8cc6cbf61d152bf5c31c0cbed2cb03dcb7445
onni2 <70693642+onni2@users.noreply.github.com>
2025-03-14 10:21:45 +0000

    adding more into battle like using weapon magic

--- battle/battle.cpp
+++ battle/battle.cpp
@@ -1,99 +1,152 @@
 #include "battle.h"
-// For color output (for supported terminals)
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+
 #define RESET_COLOR "\033[0m"
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 #define BLUE "\033[34m"
-
-// For line break and formatting
 #define LINE_BREAK "\n============================\n"
 
 void printFightStats(const Entity* player, const Entity* mob) {
-    std::cout << "Your character: " << player->getName() 
-              << " (Health: " << player->getHealth() 
-              << ", Strength: " << player->getStrength() 
+    std::cout << "Your character: " << player->getName()
+              << " (Health: " << player->getHealth()
+              << ", Strength: " << player->getStrength()
               << ", Defense: " << player->getDefense() << ")\n";
-    std::cout << "Mob: " << mob->getName() 
-              << " (Health: " << mob->getHealth() 
-              << ", Strength: " << mob->getStrength() 
+    std::cout << "Mob: " << mob->getName()
+              << " (Health: " << mob->getHealth()
+              << ", Strength: " << mob->getStrength()
               << ", Defense: " << mob->getDefense() << ")\n";
 }
+
 bool attackHits(int defenderDefense, bool& isCritical) {
     std::string input;
     int roll;
-
-    // Prompt the player for input
     std::cout << "Press 'r' to roll a d20: ";
     std::cin >> input;
 
-    // If the player pressed 'r' or just pressed Enter (empty input)
     if (input == "r" || input.empty()) {
-        roll = (rand() % 20) + 1; // Roll a d20 (1-20)
+        roll = (rand() % 20) + 1;
         std::cout << "You rolled: " << roll << "\n";
     }
 
-    if (roll == 20) { 
+    if (roll == 20) {
         isCritical = true;
-        return true; // Always hits
+        return true;
     }
 
     isCritical = false;
-    return roll > defenderDefense; // Normal hit check
+    return roll > defenderDefense;
+}
+
+void playerTurn(Entity* player, Entity* mob) {
+    std::cout << "Choose your action:\n";
+    std::cout << "1. Attack\n";
+    std::cout << "2. Heal\n";
+    if (player->getWeapon().magic != "None") {
+        std::cout << "3. Use " << player->getWeapon().magic << " magic attack\n";
+    }
+
+    int choice;
+    std::cin >> choice;
+
+    if (choice == 2) {
+        int healAmount = rand() % 5 + 1;
+        player->setHealth(player->getHealth() + healAmount);
+        std::cout << BLUE << player->getName() << " heals for " << healAmount << " HP!" << RESET_COLOR << "\n";
+    } 
+    else if (choice == 3 && player->getWeapon().magic != "null") {
+        std::string magicType = player->getWeapon().magic;
+        int magicDamage = player->getStrength() + 5; // Magic does extra damage
+        
+        mob->setHealth(mob->getHealth() - magicDamage);
+        std::cout << RED << player->getName() << " casts " << magicType 
+                  << " magic attack! Deals " << magicDamage << " damage!" 
+                  << RESET_COLOR << "\n";
+    } 
+    else {
+        bool isCritical = false;
+        if (attackHits(mob->getDefense(), isCritical)) {
+            int damage = player->getStrength();
+            if (isCritical) {
+                damage *= 2;
+                std::cout << RED << "CRITICAL HIT! DOUBLE DAMAGE!" << RESET_COLOR << "\n";
+            }
+            mob->setHealth(mob->getHealth() - damage);
+            std::cout << GREEN << player->getName() << " hits " << mob->getName()
+                      << " for " << damage << " damage! (" << mob->getHealth() << " HP left)" 
+                      << RESET_COLOR << "\n";
+        } else {
+            std::cout << RED << player->getName() << " missed the attack!" << RESET_COLOR << "\n";
+        }
+    }
 }
 
 
+void mobTurn(Entity* mob, Entity* player) {
+    int action = rand() % 3; // 0 = attack, 1 = heal, 2 = use weapon magic
+
+    if (action == 1 && mob->getHealth() < 10) { 
+        int healAmount = rand() % 5 + 1;
+        mob->setHealth(mob->getHealth() + healAmount);
+        std::cout << BLUE << mob->getName() << " heals for " << healAmount << " HP!" << RESET_COLOR << "\n";
+    } 
+    else if (action == 2 && mob->getWeapon().magic != "null") { 
+        // Magic attack if weapon has an ability
+        std::string magicType = mob->getWeapon().magic;
+        int magicDamage = mob->getStrength() + 5; // Magic attacks are stronger
+        
+        player->setHealth(player->getHealth() - magicDamage);
+        std::cout << RED << mob->getName() << " uses " << magicType 
+                  << " magic attack! Deals " << magicDamage << " damage!" 
+                  << RESET_COLOR << "\n";
+    } 
+    else {
+        // Normal attack
+        if ((rand() % 20) + 1 > player->getDefense()) {
+            int damage = mob->getStrength();
+            player->setHealth(player->getHealth() - damage);
+            std::cout << GREEN << mob->getName() << " hits " << player->getName()
+                      << " for " << damage << " damage! (" << player->getHealth() 
+                      << " HP left)" << RESET_COLOR << "\n";
+        } else {
+            std::cout << RED << mob->getName() << " missed the attack!" << RESET_COLOR << "\n";
+        }
+    }
+}
+
+
+
 void fight(Entity* player, Entity* mob) {
+    player->getWeapon().magic;
     std::cout << LINE_BREAK;
     std::cout << BLUE << "A fight begins between " << player->getName() << " and " << mob->getName() << "!" << RESET_COLOR << "\n";
     std::cout << LINE_BREAK;
 
     while (player->getHealth() > 0 && mob->getHealth() > 0) {
-        // Player's turn
         std::cout << YELLOW << player->getName() << "'s turn!" << RESET_COLOR << "\n";
+        playerTurn(player, mob);
         
-        bool isCritical = false;
-        if (attackHits(mob->getDefense(), isCritical)) {
-            int damageToMob = player->getStrength();
-            if (isCritical) {
-                damageToMob *= 2;
-                std::cout << RED << "CRITICAL HIT! DOUBLE DAMAGE!" << RESET_COLOR << "\n";
-            }
-            mob->setHealth(mob->getHealth() - damageToMob);
-            std::cout << GREEN << player->getName() << " hits " << mob->getName() 
-                      << " for " << damageToMob << " damage! (" 
-                      << mob->getHealth() << " HP left)" << RESET_COLOR << "\n";
-        } else {
-            std::cout << RED << player->getName() << " missed the attack!" << RESET_COLOR << "\n";
-        }
-
-        // Check if mob is dead
         if (mob->getHealth() <= 0) {
             std::cout << GREEN << mob->getName() << " has been defeated!" << RESET_COLOR << "\n";
             break;
         }
 
-        std::this_thread::sleep_for(std::chrono::seconds(2));  // Delay for 2 seconds
-
-        // Mob's turn
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        
         std::cout << LINE_BREAK;
         std::cout << BLUE << mob->getName() << "'s turn!" << RESET_COLOR << "\n";
-        
-        if ((rand() % 20) + 1 > player->getDefense()) {
-            int damageToPlayer = mob->getStrength();
-            player->setHealth(player->getHealth() - damageToPlayer);
-            std::cout << GREEN << mob->getName() << " hits " << player->getName() 
-                      << " for " << damageToPlayer << " damage! (" 
-                      << player->getHealth() << " HP left)" << RESET_COLOR << "\n";
-        } else {
-            std::cout << RED << mob->getName() << " missed the attack!" << RESET_COLOR << "\n";
-        }
+        mobTurn(mob, player);
 
         if (player->getHealth() <= 0) {
             std::cout << RED << player->getName() << " has been defeated! Game over." << RESET_COLOR << "\n";
             break;
         }
-
+        
         std::cout << LINE_BREAK;
         std::cout << "---- Next round ----\n";
         std::cout << LINE_BREAK;
@@ -101,23 +154,42 @@ void fight(Entity* player, Entity* mob) {
 }
 
 Entity* pickRandomMob(const std::vector<Entity*>& mobs) {
-    srand(time(0)); // Seed random number generator
-
+    srand(time(0));
     std::vector<Entity*> aliveMobs;
+    Entity* orc = nullptr;
+    Entity* dragon = nullptr;
 
-    // Filter only mobs that are still alive
+    // Separate Orc and Dragon from other mobs and add other mobs to aliveMobs
     for (Entity* mob : mobs) {
         if (mob->getHealth() > 0) {
-            aliveMobs.push_back(mob);
+            if (mob->getName() == "Orc") {
+                orc = mob;
+            } else if (mob->getName() == "Dragon") {
+                dragon = mob;
+            } else {
+                aliveMobs.push_back(mob);
+            }
         }
     }
 
-    if (aliveMobs.empty()) {
-        std::cout << "All mobs are dead! No more enemies left.\n";
-        return nullptr;
+    // If there are alive mobs other than Orc and Dragon, pick one of them
+    if (!aliveMobs.empty()) {
+        int randomIndex = rand() % aliveMobs.size();
+        return aliveMobs[randomIndex];
     }
 
-    int randomIndex = rand() % aliveMobs.size();
-    return aliveMobs[randomIndex];
+    // If the Orc is alive and no other mobs are alive, pick the Orc
+    if (orc != nullptr) {
+        return orc;
+    }
+
+    // If the Dragon is alive and Orc is dead, pick the Dragon
+    if (dragon != nullptr) {
+        return dragon;
+    }
+
+    // If no mobs are alive
+    std::cout << "All mobs are dead! No more enemies left.\n";
+    return nullptr;
 }
 
--- battle/battle.h
+++ battle/battle.h
@@ -9,5 +9,6 @@ bool attackHits(int defenderDefense);
 void printFightStats(const Entity* player, const Entity* mob);
 void fight(Entity* player, Entity* mob);
 Entity* pickRandomMob(const std::vector<Entity*>& mobs);
+int calculateDamage(int attackerStrength, int defenderDefense, bool isCritical);
 
 #endif // BATTLE_H
